4604a5743474c6955ce0fb3372c2582b2808cdf5
Ben Roberts <ben@sihnon.net>
2011-04-25 00:44:42 +0100

    Add logging to backtround task

--- source/lib/SihnonFramework/BackgroundTask.class.php
+++ source/lib/SihnonFramework/BackgroundTask.class.php
@@ -7,6 +7,7 @@ class SihnonFramework_BackgroundTask {
     }
 
     public static function run($command) {
+        SihnonFramework_LogEntry::debug(SihnonFramework_Main::instance()->log(), "Running background task: {$command} &", 'default');
         $pipes = array();
         $pid = proc_open($command . ' &', array(), $pipes);
         proc_close($pid);
