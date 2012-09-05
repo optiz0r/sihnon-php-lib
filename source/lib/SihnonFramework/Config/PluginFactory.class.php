b39b0e16a40f45fee4a27c1e4e17b864f098551d
Benjamin Roberts <benjamin.roberts@atos.net>
2012-09-05 10:50:10 +0100

    Fix path issue causing Config plugins not to be discovered

--- source/lib/SihnonFramework/Config/PluginFactory.class.php
+++ source/lib/SihnonFramework/Config/PluginFactory.class.php
@@ -5,7 +5,7 @@ class SihnonFramework_Config_PluginFactory extends Sihnon_PluginFactory {
     protected static $plugin_prefix    = 'Sihnon_Config_Plugin_';
     protected static $plugin_interface = 'SihnonFramework_Config_IPlugin';
     protected static $plugin_dir       = array(
-       SihnonFramework_Lib => 'SihnonFramework/Config/Plugin',
+       SihnonFramework_Lib => 'SihnonFramework/Config/Plugin/',
        Sihnon_Lib          => 'Sihnon/Config/Plugin/',
    );
     
